0fd917b7e96b420583e13da2854fc33ce1e0db95
Helen259 <fedorovalena2002@yandex.ru>
2022-12-16 22:24:54 +0300

    11 урок

--- js/image-scale.js
+++ js/image-scale.js
@@ -1,36 +1,44 @@
-const SCALE_MAX_VALUE = 100;
-const SCALE_MIN_VALUE = 25;
-const STEP = 25;
-const buttonBigger = document.querySelector('.scale__control--bigger');
-const buttonSmaller = document.querySelector('.scale__control--smaller');
-const scaleCounter = document.querySelector('.scale__control--value');
-const picturePreview = document.querySelector('.img-upload__preview');
-
-const setScaleOnPicture = () => {
-  const currentValue = parseFloat(scaleCounter.value);
-  picturePreview.style.transform = `scale(${currentValue / 100})`;
+const Scale = {
+  STEP: 25,
+  MAX_VALUE: 100,
+  MIN_VALUE: 25
 };
 
-const onButtonBiggerClick = () => {
-  const currentValue = parseFloat(scaleCounter.value);
-  if (currentValue < SCALE_MAX_VALUE) {
-    scaleCounter.value = `${currentValue + STEP}%`;
-    setScaleOnPicture();
+const uploadingOverlay = document.querySelector('.img-upload__overlay');
+const uploadingPicture = uploadingOverlay.querySelector('.img-upload__preview').querySelector('img');
+const scale = uploadingOverlay.querySelector('.img-upload__scale');
+const scaleCounter = scale.querySelector('.scale__control--value');
+
+const changeScale = (scaleCoefficient) => {
+  let scaleNumber = Number(scaleCounter.value.replace('%', '')) + scaleCoefficient * Scale.STEP;
+
+  if(scaleNumber < Scale.MIN_VALUE) {
+    scaleNumber = Scale.MIN_VALUE;
+  }
+  else if (scaleNumber > Scale.MAX_VALUE) {
+    scaleNumber = Scale.MAX_VALUE;
+  }
+
+  scaleCounter.value = `${scaleNumber}%`;
+  uploadingPicture.style.transform = `scale(${scaleNumber / 100})`;
+};
+
+const onScaleButtonClick = (evt) => {
+  if(evt.target.matches('button')) {
+    let coeff = 1;
+    if(evt.target.matches('.scale__control--smaller')) {
+      coeff = -1;
+    }
+
+    changeScale(coeff);
   }
 };
 
-const onButtonSmallerClick = () => {
-  const currentValue = parseFloat(scaleCounter.value);
-  if (currentValue > SCALE_MIN_VALUE) {
-    scaleCounter.value = `${currentValue - STEP}%`;
-    setScaleOnPicture();
-  }
+const scalingPhotos = () => {
+  scaleCounter.value = `${Scale.MAX_VALUE}%`;
+  uploadingPicture.style.transform = `scale(${Scale.MAX_VALUE / 100})`;
 };
 
-const resetScaleModifier = () => {
-  picturePreview.style.transform = '';
-};
+scale.addEventListener('click', onScaleButtonClick);
 
-export {resetScaleModifier};
-buttonBigger.addEventListener('click', onButtonBiggerClick);
-buttonSmaller.addEventListener('click', onButtonSmallerClick);
+export{scalingPhotos};
